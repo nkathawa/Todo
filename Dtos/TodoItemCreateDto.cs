6b7e5292459f791abd3bf11fc8f0f2cfd4603419
nkathawa <nkathawa@umich.edu>
2023-03-16 16:29:43 -0400

    cleanup

--- Dtos/TodoItemCreateDto.cs
+++ Dtos/TodoItemCreateDto.cs
@@ -4,8 +4,6 @@ namespace Todo.Dtos
 {
     public class TodoItemCreateDto
     {
-        public int Id { get; set; }
-
         public string Text { get; set; }
 
         public int UserId { get; set; }
